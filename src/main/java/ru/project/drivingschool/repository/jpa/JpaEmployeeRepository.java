50bd176140bc07bbdc2bd8171f48d038a50988d0
vladimirvolkov <angelrum@inbox.ru>
2020-11-08 02:03:40 +0300

    Подготовлены репозитории к users и empoyee

--- src/main/java/ru/project/drivingschool/repository/jpa/JpaEmployeeRepository.java
+++ src/main/java/ru/project/drivingschool/repository/jpa/JpaEmployeeRepository.java
@@ -1,6 +1,5 @@
 package ru.project.drivingschool.repository.jpa;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import ru.project.drivingschool.model.Employee;
 
 public interface JpaEmployeeRepository extends JpaNamedRepository<Employee> {
