bf40f7b38812b827be3bca0212fa3eeb2129d97f
85ctac <74019971+85ctac@users.noreply.github.com>
2020-11-11 23:15:16 +0300

    Создание ветки

--- src/main/java/ru/project/drivingschool/repository/UserRepository.java
+++ src/main/java/ru/project/drivingschool/repository/UserRepository.java
@@ -1,14 +1,18 @@
 package ru.project.drivingschool.repository;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.project.drivingschool.model.User;
 import ru.project.drivingschool.repository.jpa.JpaEmployeeRepository;
+import ru.project.drivingschool.repository.jpa.JpaKeyRepository;
 import ru.project.drivingschool.repository.jpa.JpaUserRepository;
 
 @Repository
-public class UserRepository extends AbstractNamedHistoryRepository<User> {
+@Transactional(readOnly = true)
+public class UserRepository extends AbstractRepository<User> {
 
-    public UserRepository(JpaUserRepository repository, JpaEmployeeRepository employeeRepository) {
-        super(repository, employeeRepository);
+    public UserRepository(JpaKeyRepository<User> repository) {
+        super(repository);
     }
+
 }
