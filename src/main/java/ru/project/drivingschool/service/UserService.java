ca9812e3b8e4cab763edbc67a97e92a612617268
85ctac <74019971+85ctac@users.noreply.github.com>
2020-11-11 23:09:04 +0300

    Создание ветки

--- src/main/java/ru/project/drivingschool/service/UserService.java
+++ src/main/java/ru/project/drivingschool/service/UserService.java
@@ -1,8 +1,10 @@
 package ru.project.drivingschool.service;
 
+import org.springframework.stereotype.Service;
 import ru.project.drivingschool.model.User;
 import ru.project.drivingschool.repository.AbstractRepository;
 
+@Service
 public class UserService extends AbstractService<User>{
     public UserService(AbstractRepository<User> repository) {
         super(repository);
