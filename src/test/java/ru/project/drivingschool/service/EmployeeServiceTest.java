0f0efbca78a6bca00e540210395f6c1249841558
vladimirvolkov <angelrum@inbox.ru>
2020-11-15 16:51:04 +0300

    Реализация тестов для сервиса EmployeeService. Вывод тестов сервисок в абстрактный класс.

--- src/test/java/ru/project/drivingschool/service/EmployeeServiceTest.java
+++ src/test/java/ru/project/drivingschool/service/EmployeeServiceTest.java
@@ -1,16 +1,10 @@
 package ru.project.drivingschool.service;
 
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import ru.project.drivingschool.TimingExtension;
 import ru.project.drivingschool.model.Employee;
-
-import java.util.ArrayList;
+import ru.project.drivingschool.testdata.EmployeeTestData;
 import java.util.List;
 
 import static ru.project.drivingschool.testdata.EmployeeTestData.*;
@@ -18,50 +12,28 @@ import static ru.project.drivingschool.testdata.EmployeeTestData.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
-@ExtendWith(TimingExtension.class)
-@Sql(scripts = "classpath:db/data-test.sql", config = @SqlConfig(encoding = "UTF-8"))
-class EmployeeServiceTest {
+class EmployeeServiceTest extends AbstractServiceTest<Employee> {
+
+    private EmployeeService service;
+
+    private EmployeeTestData testData;
 
     @Autowired
-    protected EmployeeService service;
+    public EmployeeServiceTest(EmployeeService service) {
+        super(service, new EmployeeTestData(), EMPLOYEE_MATCHER);
+        this.service = service;
+        this.testData = new EmployeeTestData();
+    }
 
-    //что бы сравнить schools нужно наложиь внутренние ограничения на проверки по полям createdBy и createdOn
-    //сделаем это при реализации тестов для школы, пока сравниваем по кол-ву объектов
-    @Test
+    //что бы сравнивать schools необходимо из сравнения исключить поля createdBy и createdOn
+    //это будет сделано после реализации тестов для ServiceSchools
+    @Override
     void getAll() {
+        super.getAll();
         List<Employee> actual = service.getAll();
-        EMPLOYEE_MATCHER.assertMatch(actual, EMPLOYEES);
         for (int i = 0; i < actual.size(); i++)
-            Assertions.assertThat(actual.get(i).getSchools()).hasSize(EMPLOYEES.get(i).getSchools().size());
-    }
-
-    @Test
-    void get() {
-        EMPLOYEE_MATCHER.assertMatch(service.get(EMPLOYEE_ID1), EMPLOYEE1);
-    }
-
-    @Test
-    void create() {
-        Employee employee = getNew();
-        Employee create = service.create(employee);
-        employee.setId(create.getId());
-        List<Employee> expected = new ArrayList<>(EMPLOYEES);
-        expected.add(employee);
-        EMPLOYEE_MATCHER.assertMatch(service.getAll(), expected);
-    }
-
-    @Test
-    void update() {
-        Employee expected = getUpdate();
-        service.update(expected);
-        EMPLOYEE_MATCHER.assertMatch(service.get(expected.id()), expected);
-        Assertions.assertThat(service.getAll()).hasSize(EMPLOYEES.size());
-    }
-
-    @Test
-    void delete() {
-        service.delete(EMPLOYEE_ID1);
-        EMPLOYEE_MATCHER.assertMatch(service.getAll(), List.of(EMPLOYEE2));
+            Assertions.assertThat(actual.get(i).getSchools())
+                    .hasSize(testData.getAll().get(i).getSchools().size());
     }
 
 }
--- src/test/java/ru/project/drivingschool/testdata/EmployeeTestData.java
+++ src/test/java/ru/project/drivingschool/testdata/EmployeeTestData.java
@@ -3,42 +3,56 @@ package ru.project.drivingschool.testdata;
 import ru.project.drivingschool.TestMatcher;
 import ru.project.drivingschool.model.Employee;
 import ru.project.drivingschool.model.Role;
-import ru.project.drivingschool.model.School;
 
 import java.util.*;
 
-import static ru.project.drivingschool.testdata.CompanyTestData.*;
 import static ru.project.drivingschool.testdata.SchoolTestData.*;
 
-public class EmployeeTestData {
+public class EmployeeTestData implements TestDataInterface<Employee> {
 
     public static final TestMatcher<Employee> EMPLOYEE_MATCHER =
             //TestMatcher.usingClassComparator(Employee.class, Map.of(Comparator.comparing(Employee::id), Employee.class, Comparator.comparing(School::getId), School.class), "company", "createdOn", "changedOn", "createdBy", "school");
             TestMatcher.usingFieldsComparator(Employee.class, "company", "createdOn", "changedOn", "createdBy", "schools");
+    private CompanyTestData companyData = new CompanyTestData();
 
-    public static final long EMPLOYEE_ID1 = 1_000;
-    public static final long EMPLOYEE_ID2 = 1_001;
+    private final long employeeId1 = 1_000;
+    private final long employeeId2 = 1_001;
 
     // (10000, '+7(911)111-11-11', '12345', null, 'Иванов', 'Иван', 'Иванович', 'test1@test.ru', null),
     // (10000, '+7(911)111-11-12', '123456', null, 'Иванов', 'Антон', 'Иванович', 'test2@test.ru', 1000);
 
-    public static Employee EMPLOYEE1 = new Employee(EMPLOYEE_ID1, COMPANY1, "+7(911)111-11-11", "12345", null, "Иванов", "Иван", "Иванович", "test1@test.ru", true, Employee.DEF_SCORE, null, null, null, null, Set.of(SCHOOL1, SCHOOL2), Role.ADMIN);
-    public static Employee EMPLOYEE2 = new Employee(EMPLOYEE_ID2, COMPANY1, "+7(911)111-11-12", "123456", null, "Иванов", "Антон", "Иванович", "test2@test.ru", true, Employee.DEF_SCORE, null, EMPLOYEE1, null, null, null, Role.MANAGER);
+    private Employee employee1 = new Employee(employeeId1, companyData.getObjectById(companyData.getId1()), "+7(911)111-11-11", "12345", null, "Иванов", "Иван", "Иванович", "test1@test.ru", true, Employee.DEF_SCORE, null, null, null, null, Set.of(SCHOOL1, SCHOOL2), Role.ADMIN);
+    private Employee employee2 = new Employee(employeeId2, companyData.getObjectById(companyData.getId1()), "+7(911)111-11-12", "123456", null, "Иванов", "Антон", "Иванович", "test2@test.ru", true, Employee.DEF_SCORE, null, employee1, null, null, null, Role.MANAGER);
 
-    public static List<Employee> EMPLOYEES = List.of(EMPLOYEE1, EMPLOYEE2);
-
-    public static Employee getNew() {
-        Employee emp = new Employee(EMPLOYEE1);
+    @Override
+    public Employee getNew() {
+        Employee emp = new Employee(employee2);
         emp.setId(null);
         emp.setPhone("+7(911)111-11-13");
         emp.setFirstname("Петров");
-        emp.setCreatedBy(EMPLOYEE2);
+        emp.setCreatedBy(employee2);
         return emp;
     }
 
-    public static Employee getUpdate() {
-        Employee upd = new Employee(EMPLOYEE2);
+    @Override
+    public Employee getUpdate() {
+        Employee upd = new Employee(employee2);
         upd.setPhone("+7(911)111-11-14");
         return upd;
+    }
+
+    @Override
+    public long getId1() {
+        return employeeId1;
+    }
+
+    @Override
+    public long getId2() {
+        return employeeId2;
+    }
+
+    @Override
+    public List<Employee> getAll() {
+        return new ArrayList<>(List.of(employee1, employee2));
     }
 }
