1265c0085a9ba7c783471cd501ade270f74ef060
vladimirvolkov <angelrum@inbox.ru>
2020-11-15 17:28:12 +0300

    Расширение EmployeeService

--- src/main/java/ru/project/drivingschool/repository/EmployeeRepository.java
+++ src/main/java/ru/project/drivingschool/repository/EmployeeRepository.java
@@ -4,10 +4,19 @@ import org.springframework.stereotype.Repository;
 import ru.project.drivingschool.model.Employee;
 import ru.project.drivingschool.repository.jpa.JpaEmployeeRepository;
 
+import java.util.List;
+
 @Repository
 public class EmployeeRepository extends AbstractNamedHistoryRepository<Employee> {
 
+    private JpaEmployeeRepository repository;
+
     public EmployeeRepository(JpaEmployeeRepository employeeRepository) {
         super(employeeRepository, employeeRepository);
+        this.repository = employeeRepository;
+    }
+
+    public List<Employee> getAll(long companyId) {
+        return this.repository.getAllByCompanyIdOrderById(companyId);
     }
 }
--- src/main/java/ru/project/drivingschool/service/EmployeeService.java
+++ src/main/java/ru/project/drivingschool/service/EmployeeService.java
@@ -4,10 +4,20 @@ import org.springframework.stereotype.Service;
 import ru.project.drivingschool.model.Employee;
 import ru.project.drivingschool.repository.EmployeeRepository;
 
+import java.util.List;
+
 @Service
 public class EmployeeService extends AbstractService<Employee> {
 
+    private EmployeeRepository repository;
+
     public EmployeeService(EmployeeRepository repository) {
         super(repository);
+        this.repository = repository;
     }
+
+    public List<Employee> getAll(long companyId) {
+        return this.repository.getAll(companyId);
+    }
+
 }
