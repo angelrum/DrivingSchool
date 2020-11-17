f68b20b1a2d0e6f6fc8620654819c71d898084bb
85ctac <74019971+85ctac@users.noreply.github.com>
2020-11-17 23:01:28 +0300

    Обновление UserTestService

--- src/test/java/ru/project/drivingschool/testdata/UserTestData.java
+++ src/test/java/ru/project/drivingschool/testdata/UserTestData.java
@@ -5,66 +5,59 @@ import ru.project.drivingschool.model.Company;
 import ru.project.drivingschool.model.User;
 
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 
 public class UserTestData implements TestDataInterface<User>{
-    @Override
-    public long getId1() {
-        return 0;
-    }
-
-    @Override
-    public long getId2() {
-        return 0;
-    }
-
-    @Override
-    public Collection<User> getAll() {
-        return null;
-    }
-
-    @Override
-    public User getNew() {
-        return null;
-    }
-
-    @Override
-    public User getUpdate() {
-        return null;
-    }
-/*
-    public static final TestMatcher<User> USER_TEST_MATCHER = TestMatcher.usingFieldsComparator(User.class, "createdOn");
 
     public static final long USER_ID1 = 1003;
     public static final long USER_ID2 = 1004;
-
     public static final long NOT_FOUND_ID = 99999;
-
-    public static User User1 = new User(USER_ID1, "8(911)111-11-11", "12345",
-            null, "Иван", "Иванов", "Иванович", "ivan@ivan.ru",
-            true, null, null,
-            null, null);
-    public static User User2 = new User(USER_ID2, "8(911)111-11-12", "123456",
-            null, "Сергей", "Иванов", "Иванович", "serg@ivan.ru",
-            true, null, null,
-            null, null);
+    public static final TestMatcher<User> USER_TEST_MATCHER = TestMatcher.usingFieldsComparator(User.class, "createdOn");
 
 
     public static List<User> Users = List.of(User1, User2);
 
-    public static User getNewUser() {
+    public static User User1 = new User(USER_ID1, "8(911)111-11-11", "12345",
+            null, "Иван", "Иванов", "Иванович", "ivan@ivan.ru",
+            true, null, null,
+            null, null, null);
+    public static User User2 = new User(USER_ID2, "8(911)111-11-12", "123456",
+            null, "Сергей", "Иванов", "Иванович", "serg@ivan.ru",
+            true, null, null,
+            null, null, null);
+
+
+    @Override
+    public long getId1() {
+        return USER_ID1;
+    }
+
+    @Override
+    public long getId2() {
+        return USER_ID2;
+    }
+
+    @Override
+    public Collection<User> getAll() {
+        return new ArrayList<>(List.of(User1, User2));
+    }
+
+    @Override
+    public User getNew() {
         User userNew = new User(User1);
         userNew.setId(null);
         userNew.setFirstname("Новый пользователь");
         return userNew;
     }
 
-    public static User getUpdateUser() {
+    @Override
+    public User getUpdate() {
         User upd = new User(User2);
         upd.setFirstname("Обновленный пользователь");
         return upd;
     }
 
- */
 }
